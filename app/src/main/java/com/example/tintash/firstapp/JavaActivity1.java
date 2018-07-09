3be25058a9132c0b00f048ab2545a2fb15007378
babakshahid <babakshahid@gmail.com>
2018-07-09 11:49:21 +0500

    First Activity created.

--- app/src/main/java/com/example/tintash/firstapp/JavaActivity1.java
+++ app/src/main/java/com/example/tintash/firstapp/JavaActivity1.java
@@ -9,10 +9,69 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class JavaActivity1 extends AppCompatActivity implements View.OnClickListener {
+
+    Button toast, count, random;
+    TextView count_tv;
+    int count_int = 0;
+    private static final String TOTAL_COUNT = "total_count";
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_java1);
-
+        init();
+        setter();
     }
+
+    private void init() {
+        context = getApplicationContext();
+    }
+
+
+    private void setter() {
+        count = findViewById(R.id.count);
+        random = findViewById(R.id.random);
+        toast = findViewById(R.id.toast);
+        count_tv = findViewById(R.id.tv_count);
+
+        toast.setOnClickListener(this);
+        random.setOnClickListener(this);
+        count.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.toast:
+                showToast("Babak is testing",Toast.LENGTH_SHORT);
+                break;
+            case R.id.count:
+                countInc();
+                break;
+            case R.id.random:
+                startNewActivity();
+                break;
+            default:
+                showToast("No Action is registered",Toast.LENGTH_SHORT);
+        }
+    }
+
+    private void startNewActivity() {
+        Intent newIntent = new Intent(context,JavaActivity2.class);
+        newIntent.putExtra(TOTAL_COUNT,count_int);
+        startActivity(newIntent);
+    }
+
+    private void countInc(){
+        count_int++;
+        updateUI();
+    }
+
+    private void updateUI(){
+        count_tv.setText(String.valueOf(count_int));
+    }
+
+    private void showToast(String str, int time){
+        Toast.makeText(context,str,time).show();
+    }
+
 }
---
+++ app/src/main/java/com/example/tintash/firstapp/JavaActivity2.java
@@ -0,0 +1,10 @@
+package com.example.tintash.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import java.util.Random;
+
+public class JavaActivity2 extends AppCompatActivity {
+
+}
