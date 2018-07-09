30bdc4ca93bcd4102b0f6870dee3a4594df3a761
babakshahid <babakshahid@gmail.com>
2018-07-09 11:53:21 +0500

    App clonned into kotlin.

---
+++ app/src/main/java/com/example/tintash/firstapp/KotlinActivity1.kt
@@ -0,0 +1,41 @@
+package com.example.tintash.firstapp
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_test_java1.*
+
+class KotlinActivity1 : AppCompatActivity() {
+
+    var count_int = 0
+    lateinit var context: Context
+    val TOTAL_COUNT = "total"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_java1)
+        context = this
+        setter()
+    }
+    fun setter(){
+        count.setOnClickListener {
+            count_int += 1
+            tv_count.text = count_int.toString()
+        }
+        random.setOnClickListener{ v-> startNewActivity() }
+        toast.setOnClickListener {_->
+            showToast("Test Toast", Toast.LENGTH_SHORT)
+        }
+    }
+
+    private fun startNewActivity() {
+
+        val intent = Intent(context,KotlinActivity2::class.java)
+        intent.putExtra(TOTAL_COUNT,count_int)
+        startActivity(intent)
+    }
+
+    private fun showToast(message: String, length:Int){
+        Toast.makeText(context,message,length).show()
+    }
+}
---
+++ app/src/main/java/com/example/tintash/firstapp/KotlinActivity2.kt
@@ -0,0 +1,24 @@
+package com.example.tintash.firstapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_test_java2.*
+import java.util.*
+
+class KotlinActivity2 : AppCompatActivity() {
+
+    val TOTAL_COUNT = "total"
+    var count = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_java2)
+        count = intent.getIntExtra(TOTAL_COUNT,0)
+        val r = Random()
+        var randomInt = 0
+        if (count > 0) {
+            randomInt = r.nextInt(count +1)
+        }
+        random.text = randomInt.toString()
+        header.text = getString(R.string.header, count)
+    }
+}
